8f50f0ececde56f3278fcddfa8659c7eabe65786
Sebastian Müller <info@sebastian-mueller.net>
2017-07-11 20:24:33 +0200

    fix: data fetching for list

--- src/components/list/list.component.ts
+++ src/components/list/list.component.ts
@@ -6,6 +6,8 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/combineLatest';
 import { isPlatformBrowser } from '@angular/common';
 
 @Component({
@@ -34,24 +36,24 @@ export class ListComponent {
   }
 
   private _fetchData() {
-    this._route.params.subscribe(data => {
-      this._route.queryParamMap.subscribe((queryParams) => {
+    this.content = Observable
+      .combineLatest(this._route.params, this._route.queryParamMap)
+      .switchMap(([params, queryParams]) => {
         const page = queryParams.get('page');
-        this.mode = data.mode;
+        this.mode = params.mode;
         const pageQuery = (page != null && parseInt(page) > 0) ? `?p=${page}` : '';
         this.page = page != null && parseInt(page) > 0 ? parseInt(page, 10) : 1;
-        this.content = this._http.get('/api/' + this.mode + pageQuery)
+        return this._http.get('/api/' + this.mode + pageQuery)
           .do(r => {
             if (r.headers != null) {
               const pagesHeader = r.headers.get('X-Pages');
               this.pages = pagesHeader != null ? parseInt(pagesHeader, 10) : 0;
+              const pageTitle = (<string>params.mode[0]).toUpperCase() + (<string>params.mode).substr(1, params.mode.length-1);
+              this._title.setTitle(pageTitle + ' | Angular HN');
             }
           })
-          .map(r => r.json());
-        const pageTitle = (<string>data.mode[0]).toUpperCase() + (<string>data.mode).substr(1, data.mode.length-1);
-        this._title.setTitle(pageTitle + ' | Angular HN');
-      })
-    })
+          .map(r => r.json())
+      });
   }
 
   trackByFn(_: number, item: any) {
